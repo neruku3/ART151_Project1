52a957c6e62c212e90f465c087d29eca06ccb5b1
Nikith Erukulla <neruk41@Nikiths-MBP.hsd1.il.comcast.net>
2021-01-19 14:48:54 -0600

    Rectangles change colors and follow mouse

--- sketch.js
+++ sketch.js
@@ -1,7 +1,40 @@
+let x = 50;
+let y = 30;
+let speed = 5;
+let speedy = 10;
+rectH = 50;
+rectW = 25;
+r = 255;
+g = 0;
+b = 0;
+
 function setup() {
     createCanvas(windowWidth, windowHeight);
+    background(169,169,169);
   }
   
   function draw() {
-    background(70,103,156);
+    //fill(r,g,b,20);
+    /*strokeWeight(20);
+    stroke(200,20,20,10);*/
+    /*if(x > (windowWidth-(rectW/2)) || x < (rectW/2)) {
+      speed = speed * -1;
+    }
+    if(y > (windowHeight-(rectH/2)) || y < (rectW/2)) {
+      speedy = speedy * -1;
+    }
+    x = x + speed;
+    y = y + speedy;
+    */
+  }
+
+  function mouseDragged() {
+    fill(r,g,b);
+    rect(mouseX, mouseY, rectW, rectH);
+    if (mouseIsPressed == true) {
+      r = Math.floor(Math.random() * (255 - 0) + 0);
+      g = Math.floor(Math.random() * (255 - 0) + 0);
+      b = Math.floor(Math.random() * (255 - 0) + 0);
+    }
+    return false;
   }
