4512d1331505703f99ce6b0e4d6b26ec30778626
Nikith Erukulla <neruk41@Nikiths-MBP.hsd1.il.comcast.net>
2021-01-21 15:28:39 -0600

    This commit makes it so that the drawing reflects and there are two lines. There is also a Y offset, but no X offset.

--- sketch.js
+++ sketch.js
@@ -1,40 +1,116 @@
-let x = 50;
+let x = 100;
 let y = 30;
-let speed = 5;
-let speedy = 10;
-rectH = 50;
-rectW = 25;
-r = 255;
-g = 0;
-b = 0;
+let speed = 2;
+let speedy = 2;
+let shapeHeight = 20;
+let shapeWidth = 20;
+let r = 230;
+let g = 0;
+let b = 0;
+let canvX;
+let canvY;
+let reposX;
+let reposY;
+let offsetX;
+let offsetY;
+let offsetArray = [-10,-9,-8,-7,-6,-5,-4,-3,-2,-1,0,1,2,3,4,5,6,7,8,9,10];
+let upordown;
+let indexVal = 0;
 
 function setup() {
-    createCanvas(windowWidth, windowHeight);
+    if (windowHeight > windowWidth) {
+      canvX = windowWidth;
+      canvY = windowWidth;
+    }
+    else if (windowWidth > windowHeight) {
+      canvX = windowHeight;
+      canvY = windowHeight;
+    }
+    cnv = createCanvas(canvX, canvY);
+    reposX = (windowWidth - canvX) / 2;
+    reposY = (windowHeight - canvY) / 2;
+    cnv.position(reposX,reposY);
     background(169,169,169);
   }
   
   function draw() {
-    //fill(r,g,b,20);
-    /*strokeWeight(20);
-    stroke(200,20,20,10);*/
-    /*if(x > (windowWidth-(rectW/2)) || x < (rectW/2)) {
+    fill(r,g,b);
+    //strokeWeight(10);
+    //stroke(200,20,20);
+    offsetX = offsetArray[indexVal];
+    offsetY = offsetArray[indexVal];
+    //ellipse(x,y,shapeHeight,shapeWidth);
+    rect(x, y+offsetY, 10, 5);
+    //ellipse(y,x,shapeHeight,shapeWidth);
+    rect(y+offsetY, x, 10, 5);
+    strokeWeight(3);
+    stroke(169,169,169, 100);
+    smooth();
+    if(x > (canvX-(shapeWidth/2)) || x < (shapeWidth/2)) {
       speed = speed * -1;
     }
-    if(y > (windowHeight-(rectH/2)) || y < (rectW/2)) {
+    if(y > (canvY-(shapeHeight/2)) || y < (shapeWidth/2)) {
       speedy = speedy * -1;
     }
     x = x + speed;
     y = y + speedy;
-    */
+
+    if (indexVal == 0) {
+      upordown = 1;
+    }
+    else if (indexVal == 20) {
+      upordown = -1;
+    }
+    indexVal = indexVal + upordown;
+    
   }
 
+  function mouseClicked() {
+    x = mouseX;
+    y = mouseY;
+    // prevent default
+    return false;
+  }
+
+  function keyPressed() {
+    if (keyCode === UP_ARROW) {
+      if (speed > 0) {
+        speed = speed + 1;
+      }
+      else if (speed < 0) {
+        speed = speed - 1;
+      }
+      if (speedy > 0) {
+        speedy = speedy + 1;
+      }
+      else if (speedy < 0) {
+        speedy = speedy - 1;
+      }
+    }
+    else if (keyCode === DOWN_ARROW) {
+      if (speed > 0 && (speed - 1) > 0) {
+        speed = speed - 1;
+      }
+      else if (speed < 0 && (speed + 1) < 0) {
+        speed = speed + 1;
+      }
+      if (speedy > 0 && (speedy - 1) > 0) {
+        speedy = speedy - 1;
+      }
+      else if (speedy < 0 && (speedy + 1) < 0) {
+        speedy = speedy + 1;
+      }
+    }
+  }
+  /*
   function mouseDragged() {
     fill(r,g,b);
-    rect(mouseX, mouseY, rectW, rectH);
+    rect(mouseX, mouseY, width, height);
     if (mouseIsPressed == true) {
-      r = Math.floor(Math.random() * (255 - 0) + 0);
-      g = Math.floor(Math.random() * (255 - 0) + 0);
-      b = Math.floor(Math.random() * (255 - 0) + 0);
+      r = Math.floor(Math.random() * (255 - 0) + 200);
+      g = Math.floor(Math.random() * (255 - 0) + 200);
+      b = Math.floor(Math.random() * (255 - 0) + 200);
     }
     return false;
   }
+  */
