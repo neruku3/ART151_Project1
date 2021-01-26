39e2f3a50b15d598e1f17b6acdd273dfbd8216f5
Nikith Erukulla <neruk41@Nikiths-MBP.hsd1.il.comcast.net>
2021-01-26 15:25:48 -0600

    Changed color scheme and made marks more smooth

--- sketch.js
+++ sketch.js
@@ -2,11 +2,8 @@ let x = 100;
 let y = 30;
 let speed = 1;
 let speedy = 1;
-let shapeHeight = 20;
-let shapeWidth = 20;
-let r = 230;
-let g = 0;
-let b = 0;
+let shapeHeight = 5;
+let shapeWidth = 10;
 let canvX;
 let canvY;
 let reposX;
@@ -17,6 +14,7 @@ let offsetArrayY = [-10,-9,-8,-7,-6,-5,-4,-3,-2,-1,0,1,2,3,4,5,6,7,8,9,10];
 let offsetArrayX = [10,9,8,7,6,5,4,3,2,1,0,-1,-2,-3,-4,-5,-6,-7,-8,-9,-10];
 let upordown;
 let indexVal = 0;
+let frameVal = 60;
 
 function setup() {
     if (windowHeight > windowWidth) {
@@ -31,26 +29,30 @@ function setup() {
     reposX = (windowWidth - canvX) / 2;
     reposY = (windowHeight - canvY) / 2;
     cnv.position(reposX,reposY);
-    background(169,169,169);
+    background(204,153,44);
   }
   
   function draw() {
-    fill(r,g,b);
-    //strokeWeight(10);
-    //stroke(200,20,20);
+    frameRate(frameVal);
     offsetX = offsetArrayX[indexVal];
     offsetY = offsetArrayY[indexVal];
-    //ellipse(x,y,shapeHeight,shapeWidth);
-    rect(x, y+offsetY, 10, 5);
-    //ellipse(y,x,shapeHeight,shapeWidth);
-    rect(y+offsetY, x, 10, 5);
+    fill(216,201,170);
     strokeWeight(3);
-    stroke(169,169,169, 100);
+    stroke(216,201,170, 150);
+    //ellipse(x,y,shapeHeight,shapeWidth);
     smooth();
+    rect(x, y+offsetY, shapeWidth, shapeHeight);
+
+    fill(157,33,25);
+    strokeWeight(3);
+    stroke(157,33,25, 150);
+    smooth();
+    //ellipse(y,x,shapeHeight,shapeWidth);
+    rect(y+offsetY, x, shapeWidth, shapeHeight);
     if(x > (canvX-(shapeWidth/2)) || x < (shapeWidth/2)) {
       speed = speed * -1;
     }
-    if(y > (canvY-(shapeHeight/2)) || y < (shapeWidth/2)) {
+    if(y > (canvY-(shapeHeight/2)) || y < (shapeHeight/2)) {
       speedy = speedy * -1;
     }
     x = x + speed;
@@ -63,18 +65,32 @@ function setup() {
       upordown = -1;
     }
     indexVal = indexVal + upordown;
-    
+
   }
 
   function mouseClicked() {
-    x = mouseX;
-    y = mouseY;
+    if (mouseX > 0 && mouseX < canvX && mouseY > 0 && mouseY < canvY) {
+        x = mouseX;
+        y = mouseY;
+    }
     // prevent default
     return false;
   }
 
   function keyPressed() {
-    if (keyCode === UP_ARROW) {
+    /*
+    if (keyCode == UP_ARROW) {
+        if (frameVal + 10 < 250) {
+          frameVal = frameVal + 10;
+        }
+    }
+    else if (keyCode == DOWN_ARROW) {
+        if (frameVal - 10 > 30) {
+            frameVal = frameVal - 10;
+        }
+    }
+    */
+    if (keyCode == UP_ARROW) {
       if (speed > 0) {
         speed = speed + 1;
       }
@@ -88,7 +104,7 @@ function setup() {
         speedy = speedy - 1;
       }
     }
-    else if (keyCode === DOWN_ARROW) {
+    else if (keyCode == DOWN_ARROW) {
       if (speed > 0 && (speed - 1) > 0) {
         speed = speed - 1;
       }
@@ -103,15 +119,3 @@ function setup() {
       }
     }
   }
-  /*
-  function mouseDragged() {
-    fill(r,g,b);
-    rect(mouseX, mouseY, width, height);
-    if (mouseIsPressed == true) {
-      r = Math.floor(Math.random() * (255 - 0) + 200);
-      g = Math.floor(Math.random() * (255 - 0) + 200);
-      b = Math.floor(Math.random() * (255 - 0) + 200);
-    }
-    return false;
-  }
-  */
